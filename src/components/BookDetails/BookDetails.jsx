b31aaa928c6924fed4bcf0812ab7f90427dbb6fd
erlopez11 <lopezelainy@gmail.com>
2025-04-05 19:05:22 -0500

    wip:fetch bookData & render book details

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,6 +1,35 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { show } from '../../services/bookService';
+
+
 const BookDetails = () => {
+    const [book, setBook] = useState(null);
+    const { bookId } = useParams();
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const bookData = await show(bookId);
+            setBook(bookData);
+        };
+        fetchBook();
+    }, [bookId]);
+
+    if (!book) return <main>Loading...</main>
+
     return (
-        <main>Book Details</main>
+        <main>
+            <section>
+                <img src={book.thumbnailUrl} />
+                <h1>{book.title}</h1>
+                <h2>{book.author}</h2>
+                <p>ISBN: {book.isbn}</p>
+                <p>Page Count: {book.numberOfPages}</p>
+                <div>
+                    {book.description}
+                </div>
+            </section>
+        </main>
     );
 };
 
