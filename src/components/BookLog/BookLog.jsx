0e8a6496610479238f7cc45e76cbcf3ea3a6600f
erlopez11 <lopezelainy@gmail.com>
2025-04-07 14:14:14 -0500

    wip: add to= for Edit Link

--- src/components/BookLog/BookLog.jsx
+++ src/components/BookLog/BookLog.jsx
@@ -1,14 +1,13 @@
 import { Link } from 'react-router';
 
-const BookLog = ({bookLog: {book, status, notes, rating}, handleDeleteBookLog}) => {
+const BookLog = ({bookLog: {book, status, notes, rating, _id}, handleDeleteBookLog, bookId}) => {
     return (
         <section>
             <h2>{book} Log </h2>
             <p>Status: {status}</p>
             <p>Notes: {notes}</p>
             <p>Rating: {rating}</p>
-            {/* TODO: Add to= with link to the edit route for the form */}
-            <Link>Edit</Link> 
+            <Link to={`/books/${bookId}/bookLog/${_id}/edit`}>Edit</Link> 
             <button onClick={handleDeleteBookLog}>Delete</button>
         </section>
     );
