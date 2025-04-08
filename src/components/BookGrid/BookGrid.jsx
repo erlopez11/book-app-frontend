84f82e6998a40167c002864b1aacdce69862afed
Pratiksha Pai <1312pratiksha@gmail.com>
2025-04-07 22:15:33 -0700

    Add BookGrid.jsx

---
+++ src/components/BookGrid/BookGrid.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect, useState } from "react";
+import BookCard from "../BookCard/BookCard";
+import { UserContext } from "../../contexts/UserContext";
+
+const BookGrid = () => {
+  const [books, setBooks] = useState([]);
+  const { user } = useContext(UserContext);
+  useEffect(() => {
+    fetch(`/books`)
+      .then((res) => res.json())
+      .then((data) => setBooks(data))
+      .catch((err) => console.error("Error fetching books:", err));
+  }, [user]);
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookCard
+          key={book.isbn}
+          title={book.title}
+          author={book.author}
+          thumbnailUrl={book.thumbnailUrl}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default BookGrid;
