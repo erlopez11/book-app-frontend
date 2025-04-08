c49695e2d0e0eb8c7f0f00e768788d97b75ad06b
erlopez11 <lopezelainy@gmail.com>
2025-04-07 19:03:42 -0500

    wip:set up collectionDetails component

---
+++ src/components/CollectionDetails/CollectionDetails.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router';
+
+const CollectionDetails = () => {
+    return (
+        <main>
+            <header>
+                <h1></h1>
+                <p></p>
+                {/* TODO: ADD to= for edit LINK */}
+                <Link>Edit</Link>
+                {/* TODO: ADD onClick to delete */}
+                <button>Delete</button>
+
+            </header>
+
+            <section>
+
+            </section>
+
+        </main>
+    );
+}
+
+export default CollectionDetails;
