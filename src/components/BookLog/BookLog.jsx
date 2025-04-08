fd12598d5b42591698bc3abf9c4b3c1c457f051a
erlopez11 <lopezelainy@gmail.com>
2025-04-08 12:02:59 -0500

    wip: add collections & add reading to status p tag

--- src/components/BookLog/BookLog.jsx
+++ src/components/BookLog/BookLog.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router';
 
-const BookLog = ({bookLog: {book, status, notes, rating, _id}, handleDeleteBookLog, bookId}) => {
+const BookLog = ({bookLog: {book, status, notes, rating, _id, collections}, handleDeleteBookLog, bookId}) => {
     return (
         <section>
             <h2>{book} Log </h2>
-            <p>Status: {status}</p>
+            <p>Reading Status: {status}</p>
             <p>Notes: {notes}</p>
             <p>Rating: {rating}</p>
+            <p>Collection: {collections}</p>
             <Link to={`/books/${bookId}/bookLog/${_id}/edit`}>Edit</Link> 
             <button onClick={handleDeleteBookLog}>Delete</button>
         </section>
