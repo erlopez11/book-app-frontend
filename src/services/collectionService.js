f4188842f6a317310763ca1ca4a5bff90e7e1d9b
erlopez11 <lopezelainy@gmail.com>
2025-04-09 17:44:25 -0500

    wip:add index, show, & delete service fn for collection

---
+++ src/services/collectionService.js
@@ -0,0 +1,47 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/collections`;
+
+const indexCollection = async () => {
+    try {
+        const res = await fetch(BASE_URL, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+        });
+        return res.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const showCollection = async (collectionId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${collectionId}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+        });
+        return res.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const deleteCollection = async (collectionId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${collectionId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+        });
+        return res.json('Delete Successful!');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export {
+    showCollection,
+    indexCollection,
+    deleteCollection,
+}
