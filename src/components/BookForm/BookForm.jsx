fb8aeaa98f261a4a727e63c2169ba71c292f9cac
Pratiksha Pai <1312pratiksha@gmail.com>
2025-04-10 01:19:13 -0700

    Add Collections dropdown to BookForm

--- src/components/BookForm/BookForm.jsx
+++ src/components/BookForm/BookForm.jsx
@@ -1,100 +1,128 @@
-import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router';
-import { showBookLog } from '../../services/bookService';
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+import { showBookLog } from "../../services/bookService";
+import { getCollections } from "../../services/collectionService";
 
 const BookForm = (props) => {
-    const { bookId, bookLogId } = useParams();
-    const [formData, setFormData] = useState({
-        book: props.book.title,
-        status: '',
-        notes: '',
-        rating: '',
-    });
-    const navigate = useNavigate();
+  const { bookId, bookLogId } = useParams();
+  const [formData, setFormData] = useState({
+    book: props.book.title,
+    status: "",
+    notes: "",
+    rating: "",
+  });
+  const [collections, setCollections] = useState([]);
+  const navigate = useNavigate();
 
-    const handleChange = (event) => {
-        setFormData({ ...formData, [event.target.name]: event.target.value });
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (bookId && bookLogId) {
+      props.handleUpdateBookLog(bookId, bookLogId, formData);
+      navigate(`/books/${bookId}`);
+    } else {
+      props.handleAddBookLog(formData);
+    }
+  };
+
+  useEffect(() => {
+    const fetchBookLog = async () => {
+      const bookLogData = await showBookLog(bookId, bookLogId);
+      setFormData(bookLogData);
     };
+    if (bookId && bookLogId) fetchBookLog();
+  }, [bookId, bookLogId]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        if (bookId && bookLogId) {
-            props.handleUpdateBookLog(bookId, bookLogId, formData);
-            navigate(`/books/${bookId}`);
-        } else {
-            props.handleAddBookLog(formData);
-        }
+  useEffect(() => {
+    const fetchCollections = async () => {
+      const response = await getCollections();
+      setCollections(response);
     };
+    fetchCollections();
+  });
 
-    useEffect(()=> {
-        const fetchBookLog = async () => {
-            const bookLogData = await showBookLog(bookId, bookLogId);
-            setFormData(bookLogData);
-        };
-        if (bookId && bookLogId) fetchBookLog();
-    }, [bookId, bookLogId]);
+  return (
+    <>
+      <h2>Add Book:</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="book">Book:</label>
+          <input
+            required
+            type="text"
+            name="book"
+            id="book"
+            value={formData.book}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="status">Add To Collection :</label>
+          <select
+            required
+            name="collection"
+            id="collection"
+            value={formData.collection}
+            onChange={handleChange}
+          >
+            <option>Select Collection</option>
+            {collections.map((collection) => (
+              <option key={collection.id} value={collection.id}>
+                {collection.title}
+              </option>
+            ))}
+          </select>
+        </div>
 
-    return (
-        <>
-            <h2>Add Book:</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='book'>Book:</label>
-                    <input
-                        required
-                        type='text'
-                        name='book'
-                        id='book'
-                        value={formData.book}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor='status'>Reading Status:</label>
-                    <select
-                        required
-                        name='status'
-                        id='status'
-                        value={formData.status}
-                        onChange={handleChange}
-                    >
-                        <option>Select Reading Status</option>
-                        <option value='want to read'>Want To Read</option>
-                        <option value='currently reading'>Currently Reading</option>
-                        <option value='read'>Read</option>
-                        <option value='did not finish'>Did Not Finish</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor='notes'>Notes:</label>
-                    <textarea
-                        required
-                        name='notes'
-                        id='notes'
-                        value={formData.notes}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor='rating'>Rating:</label>
-                    <input
-                        required
-                        type='text'
-                        name='rating'
-                        id='rating'
-                        value={formData.rating}
-                        onChange={handleChange}
-                    />
-                </div>
-                {bookLogId ? (
-                    <button type='submit'>Edit Log</button>
-                ) : (
-                    <button type='submit'>Add {props.book.title}</button>
-                )}
-            </form>
-        </>
-    );
+        <div>
+          <label htmlFor="status">Reading Status:</label>
+          <select
+            required
+            name="status"
+            id="status"
+            value={formData.status}
+            onChange={handleChange}
+          >
+            <option>Select Reading Status</option>
+            <option value="want to read">Want To Read</option>
+            <option value="currently reading">Currently Reading</option>
+            <option value="read">Read</option>
+            <option value="did not finish">Did Not Finish</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="notes">Notes:</label>
+          <textarea
+            required
+            name="notes"
+            id="notes"
+            value={formData.notes}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="rating">Rating:</label>
+          <input
+            required
+            type="text"
+            name="rating"
+            id="rating"
+            value={formData.rating}
+            onChange={handleChange}
+          />
+        </div>
+        {bookLogId ? (
+          <button type="submit">Edit Log</button>
+        ) : (
+          <button type="submit">Add {props.book.title}</button>
+        )}
+      </form>
+    </>
+  );
 };
 
 export default BookForm;
