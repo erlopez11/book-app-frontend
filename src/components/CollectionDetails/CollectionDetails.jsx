9c1dc79f1f75b63e39f79a795b984ec8f558eb5a
erlopez11 <lopezelainy@gmail.com>
2025-04-09 23:06:58 -0500

    wip:render books with collection data that matches collection title

--- src/components/CollectionDetails/CollectionDetails.jsx
+++ src/components/CollectionDetails/CollectionDetails.jsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router';
 import { showCollection } from '../../services/collectionService';
+import BookCard from '../BookCard/BookCard';
 
 const CollectionDetails = (props) => {
     const [collection, setCollection] = useState(null);
+    const [collectionBooks, setCollectionBooks] = useState([]);
     const { collectionId } = useParams();
 
     useEffect(() => {
         const fetchCollection = async () => {
             const collectionData = await showCollection(collectionId);
             setCollection(collectionData);
+            const books = collectionData.users[0].bookLog;
+            const filteredBooks = books.filter((book) => (collectionData.title === book.collections))
+            setCollectionBooks(filteredBooks);
         };
         fetchCollection();
     }, [collectionId]);
 
     if (!collection) return <main>Loading...</main>
 
-
     return (
         <main>
             <header>
                 <h1>{collection.title}</h1>
                 <p>{collection.description}</p>
-            
+
                 <Link to={`/collections/${collectionId}/edit`}>Edit</Link>
 
                 <button onClick={() => props.handleDeleteCollection(collectionId)}>Delete</button>
@@ -30,7 +34,14 @@ const CollectionDetails = (props) => {
             </header>
 
             <section>
-
+                {collectionBooks.map((book) => (
+                    <BookCard
+                        key={book._id}
+                        title={book.book}
+                        author={book.author}
+                        thumbnailUrl={book.thumbnailUrl}
+                    />
+                ))}
             </section>
 
         </main>
