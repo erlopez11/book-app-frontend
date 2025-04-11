e11c17dac6a98881c118eb14f2a7a252c295ec6f
erlopez11 <lopezelainy@gmail.com>
2025-04-10 22:26:59 -0500

    wip: add style , refactor book log html for better styling, & import book log css

--- src/components/BookLog/BookLog.jsx
+++ src/components/BookLog/BookLog.jsx
@@ -1,15 +1,32 @@
 import { Link } from 'react-router';
+import './BookLog.css';
 
-const BookLog = ({bookLog: {book, status, notes, rating, _id, collections}, handleDeleteBookLog, bookId}) => {
+const BookLog = ({ bookLog: { book, status, notes, rating, _id, collection }, handleDeleteBookLog, bookId }) => {
     return (
-        <section>
+        <section className='book-log-container'>
             <h2>{book} Log </h2>
-            <p>Status: {status}</p>
-            <p>Notes: {notes}</p>
-            <p>Rating: {rating}</p>
-            <p>Collection: {collections}</p>
-            <Link to={`/books/${bookId}/bookLog/${_id}/edit`}>Edit</Link> 
-            <button onClick={handleDeleteBookLog}>Delete</button>
+            <div className='book-log-details'>
+                <div>
+                    <p><span className='bold-text'>Reading Status:</span></p>
+                    <p>{status}</p>
+                </div>
+                <div>
+                    <p><span className='bold-text'>Notes:</span></p>
+                    <p>{notes}</p>
+                </div>
+                <div>
+                    <p><span className='bold-text'>Rating:</span></p>
+                    <p>{rating}</p>
+                </div>
+                <div>
+                    <p><span className='bold-text'>Collection:</span></p>
+                    <p>{collection}</p>
+                </div>
+
+                <Link className="link-btn" to={`/books/${bookId}/bookLog/${_id}/edit`}>Edit</Link>
+                <button className='delete-btn' onClick={handleDeleteBookLog}>Delete</button>
+
+            </div>
         </section>
     );
 };
