bdbfbbfe915e23e8c9a444afe8dabe5fc2d89a84
Pratiksha Pai <1312pratiksha@gmail.com>
2025-04-10 22:37:03 -0700

    Use collection.books instead of redundant collectionBooks variable. Fix CollectionDetails not showing books

--- src/components/CollectionDetails/CollectionDetails.jsx
+++ src/components/CollectionDetails/CollectionDetails.jsx
@@ -1,58 +1,58 @@
-import { useEffect, useState } from 'react';
-import { Link, useParams, useSearchParams } from 'react-router';
-import { getCollection } from '../../services/collectionService';
-import BookCard from '../BookCard/BookCard';
-import BookGrid from '../BookGrid/BookGrid';
+import { useEffect, useState } from "react";
+import { Link, useParams, useSearchParams } from "react-router";
+import { getCollection } from "../../services/collectionService";
+import BookCard from "../BookCard/BookCard";
+import BookGrid from "../BookGrid/BookGrid";
 
 const CollectionDetails = (props) => {
-    const [collection, setCollection] = useState(null);
-    const [collectionBooks, setCollectionBooks] = useState([]);
-    const { collectionId } = useParams();
-    const [searchParams] = useSearchParams();
-    const searchQuery = searchParams.get("q");
+  const [collection, setCollection] = useState(null);
+  const { collectionId } = useParams();
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get("q");
 
-    useEffect(() => {
-        const fetchCollection = async () => {
-            const collectionData = await getCollection(collectionId);
-            setCollection(collectionData);
-        };
-        fetchCollection();
-    }, [collectionId]);
+  useEffect(() => {
+    const fetchCollection = async () => {
+      const collectionData = await getCollection(collectionId);
+      setCollection(collectionData);
+    };
+    fetchCollection();
+  }, [collectionId]);
 
-    if (!collection) return <main>Loading...</main>
+  if (!collection) return <main>Loading...</main>;
 
-    return (
-        <main>
-            {searchQuery ? (
-                // If there's a search query, show the BookGrid component
-                <BookGrid />
-            ) : (
-                // Otherwise, show the collection details
-                <>
-                    <header>
-                        <h1>{collection.title}</h1>
-                        <p>{collection.description}</p>
+  return (
+    <main>
+      {searchQuery ? (
+        // If there's a search query, show the BookGrid component
+        <BookGrid />
+      ) : (
+        // Otherwise, show the collection details
+        <>
+          <header>
+            <h1>{collection.title}</h1>
+            <p>{collection.description}</p>
 
-                        <Link to={`/collections/${collectionId}/edit`}>Edit</Link>
+            <Link to={`/collections/${collectionId}/edit`}>Edit</Link>
 
-                        <button onClick={() => props.handleDeleteCollection(collectionId)}>Delete</button>
+            <button onClick={() => props.handleDeleteCollection(collectionId)}>
+              Delete
+            </button>
+          </header>
 
-                    </header>
-
-                    <section>
-                        {collectionBooks.map((book) => (
-                            <BookCard
-                                key={book._id}
-                                title={book.book}
-                                author={book.author}
-                                thumbnailUrl={book.thumbnailUrl}
-                            />
-                        ))}
-                    </section>
-                </>
-            )}
-        </main>
-    );
-}
+          <section>
+            {collection.books.map((book) => (
+              <BookCard
+                key={book._id}
+                title={book.book}
+                author={book.author}
+                thumbnailUrl={book.thumbnailUrl}
+              />
+            ))}
+          </section>
+        </>
+      )}
+    </main>
+  );
+};
 
 export default CollectionDetails;
